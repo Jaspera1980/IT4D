3220b556bbfcbadb38278ec0b0ef514b05a4a13a
Jaspera1980 <jasper_1980@live.nl>
2018-04-20 17:31:15 +0200

    Writing your first Django app, part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -40,5 +40,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
-
-####asfsdg
