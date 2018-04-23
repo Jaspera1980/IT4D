0664e954c4b5001b429bf89d1fa76244d5f45024
Jaspera1980 <jasper_1980@live.nl>
2018-04-23 11:57:34 +0200

    Writing my own app

--- mysite/AniReg/urls.py
+++ mysite/AniReg/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#    path('', views.index, name='index'),
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    path('', views.index, name='index'),
+#    url(r'^$', views.post_list, name='post_list'),
+#    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+#    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
