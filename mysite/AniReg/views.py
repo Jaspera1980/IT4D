045c665bc20f4b5de7f6a4078e47b7c9e77fdfe6
Jaspera1980 <jasper_1980@live.nl>
2018-04-23 15:01:02 +0200

    Writing my own app

--- mysite/AniReg/views.py
+++ mysite/AniReg/views.py
@@ -1,19 +1,4 @@
-
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def AniRegForm(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-#from .forms import PostForm
-
-#def post_new(request):
-#    form = PostForm()
-#    return render(request, 'blog/post_edit.html', {'form': form})
-
 from django.shortcuts import render
 
-#def AniRegForm(request):
-#    return render(request, 'AniReg/index.html')
+def AniRegForm(request):
+    return render(request, 'AniReg/AniRegForm.html', {})
